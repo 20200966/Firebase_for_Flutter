e50ea812f744fa5ec5aea66db88d51375c22e79e
20200966 <20200966@miucateci.edu.do>
2024-11-19 13:43:57 -0400

    fix: home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -22,17 +22,20 @@ class HomePage extends StatelessWidget {
         children: <Widget>[
           Image.asset('assets/codelab.png'),
           const SizedBox(height: 8),
-          const IconAndDetail(Icons.calendar_today, 'October 30'),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) =>
+                IconAndDetail(Icons.calendar_today, appState.eventDate),
+          ),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
-        
           Consumer<ApplicationState>(
             builder: (context, appState, _) => AuthFunc(
-                loggedIn: appState.loggedIn,
-                signOut: () {
-                  FirebaseAuth.instance.signOut();
-                }),
+              loggedIn: appState.loggedIn,
+              signOut: () {
+                FirebaseAuth.instance.signOut();
+              },
+              enableFreeSwag: appState.enableFreeSwag,
+            ),
           ),
-        
           const Divider(
             height: 8,
             thickness: 1,
@@ -41,39 +44,35 @@ class HomePage extends StatelessWidget {
             color: Colors.grey,
           ),
           const Header("What we'll be doing"),
-          const Paragraph(
-            'Join us for a day full of Firebase Workshops and Pizza!',
-          ),
-          
           Consumer<ApplicationState>(
-          builder: (context, appState, _) => Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-      
-            switch (appState.attendees) {
-            1 => const Paragraph('1 person going'),
-            >= 2 => Paragraph('${appState.attendees} people going'),
-            _ => const Paragraph('No one going'),
-            },
-      
-            if (appState.loggedIn) ...[
-        
-            YesNoSelection(
-            state: appState.attending,
-            onSelection: (attending) => appState.attending = attending,
+            builder: (context, appState, _) => Paragraph(
+              appState.callToAction,
             ),
-        
-            const Header('Discussion'),
-            GuestBook(
-             addMessage: (message) =>
-              appState.addMessageToGuestBook(message),
-              messages: appState.guestBookMessages,
+          ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                switch (appState.attendees) {
+                  1 => const Paragraph('1 person going'),
+                  >= 2 => Paragraph('${appState.attendees} people going'),
+                  _ => const Paragraph('No one going'),
+                },
+                if (appState.loggedIn) ...[
+                  YesNoSelection(
+                    state: appState.attending,
+                    onSelection: (attending) => appState.attending = attending,
+                  ),
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                    messages: appState.guestBookMessages,
+                  ),
+                ],
+              ],
             ),
-          ],
-        ],
-      ),
-    ),
-          
+          ),
         ],
       ),
     );
